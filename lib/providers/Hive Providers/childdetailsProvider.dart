c6498f648b919d4927482a17314e8b1df47ee532
adeshpokhrel15 <adeshpokhrel15@gmail.com>
2022-06-24 12:39:51 +0545

    child health Hive provider

---
+++ lib/providers/Hive Providers/childdetailsProvider.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:palika/Hive/addresshive.dart';
+import 'package:palika/Hive/appearencehive.dart';
+import 'package:palika/Hive/childhlthhive.dart';
+import 'package:palika/Hive/childrenhive.dart';
+import 'package:palika/main.dart';
+
+final childdetailModelProvider = StateNotifierProvider<
+        ChilddetailHiveModelProvider, List<ChildDetailsHiveModel>>(
+    (ref) => ChilddetailHiveModelProvider(ref: ref));
+
+class ChilddetailHiveModelProvider
+    extends StateNotifier<List<ChildDetailsHiveModel>> {
+  ChilddetailHiveModelProvider({required this.ref})
+      : super(ref.read(boxChilddetails));
+  StateNotifierProviderRef ref;
+
+  void addForm(ChildDetailsHiveModel useform) async {
+    final boxAppearence =
+        await Hive.openBox<ChildDetailsHiveModel>('childdetailshivemodel');
+    if (state.isEmpty) {
+      final useForm = ChildDetailsHiveModel();
+
+      state = [...state, useForm];
+    }
+  }
+
+  void clearAll() {
+    Hive.box<ChildDetailsHiveModel>('childdetailshivemodel').clear();
+  }
+
+  @override
+  void dispose() {
+    Hive.box('childdetailshivemodel').close();
+    super.dispose();
+  }
+}
---
+++ lib/providers/Hive Providers/childhealthProvider.dart
@@ -0,0 +1,56 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:palika/Hive/addresshive.dart';
+import 'package:palika/Hive/appearencehive.dart';
+import 'package:palika/Hive/childhlthhive.dart';
+import 'package:palika/main.dart';
+
+final childhealthModelProvider = StateNotifierProvider<
+        ChildhealthHiveModelProvider, List<ChildHealthHiveModel>>(
+    (ref) => ChildhealthHiveModelProvider(ref: ref));
+
+class ChildhealthHiveModelProvider
+    extends StateNotifier<List<ChildHealthHiveModel>> {
+  ChildhealthHiveModelProvider({required this.ref})
+      : super(ref.read(boxChildhealth));
+  StateNotifierProviderRef ref;
+
+  void addForm(ChildHealthHiveModel useform) async {
+    final boxAppearence =
+        await Hive.openBox<ChildHealthHiveModel>('childhealthhivemodel');
+    if (state.isEmpty) {
+      final useForm = ChildHealthHiveModel(
+        childrenbirthcondition: useform.childrenbirthcondition,
+        childrenbirthplace: useform.childrenbirthplace,
+        childrenbloodgroup: useform.childrenbloodgroup,
+        childrengeneticdiseasedescription:
+            useform.childrengeneticdiseasedescription,
+            childrenisBCGvaccinated: useform.childrenisBCGvaccinated,
+            childrenisDPTHEPBvaccinated: useform.childrenisDPTHEPBvaccinated,
+            childrenisIPVvaccinated: useform.childrenisIPVvaccinated,
+            childrenisJEvaccinated: useform.childrenisJEvaccinated,
+            childrenisMRvaccinated: useform.childrenisMRvaccinated,
+            childrenisOPVvaccinated: useform.childrenisOPVvaccinated,
+            childrenisPCVvaccinated: useform.childrenisPCVvaccinated,
+            childrenisTDvaccinated: useform.childrenisTDvaccinated,
+            childreniscovidvaccinated: useform.childreniscovidvaccinated,
+            childrenisgeneticdiseaseissue: useform.childrenisgeneticdiseaseissue,
+            childrenvaccinedetails: useform.childrenvaccinedetails,
+            childrenvacinedose: useform.childrenvacinedose,
+            chilrenbirthweight: useform.chilrenbirthweight,
+      );
+
+      state = [...state, useForm];
+    }
+  }
+
+  void clearAll() {
+    Hive.box<ChildHealthHiveModel>('childhealthhivemodel').clear();
+  }
+
+  @override
+  void dispose() {
+    Hive.box('childhealthhivemodel').close();
+    super.dispose();
+  }
+}
